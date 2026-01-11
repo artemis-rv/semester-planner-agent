486ab5fcc77bf5a69a4273ba21c1ed5f1d889ee9
artemis-rv <fjayaanm07@gmail.com>
2026-01-11 12:03:02 +0530

    added planning structure logic

---
+++ planner/engine/planner_engine.py
@@ -0,0 +1,42 @@
+from planner.models.syllabus import Subject
+
+
+class PlannerEngine:
+    """
+    Converts syllabus data into a flat planning structure.
+    
+    """
+
+    def __init__(self, subjects: list[Subject]):
+        self.subjects = subjects
+
+    def generate_plan(self) -> list[dict]:
+        plan_rows = []
+
+        for subject in self.subjects:
+            for unit in subject.units:
+                for topic in unit.topics:
+                    plan_rows.append({
+                        "subject": subject.name,
+                        "unit": unit.unit_no,
+                        "unit_title": unit.title,
+                        "importance": unit.importance,
+                        "topic": topic.topic,
+                        "subtopics": ", ".join(topic.subtopics),
+                        "minimum_hours": unit.minimum_hours,
+                        "self_study": False
+                    })
+
+                for ss in unit.self_study:
+                    plan_rows.append({
+                        "subject": subject.name,
+                        "unit": unit.unit_no,
+                        "unit_title": unit.title,
+                        "importance": unit.importance,
+                        "topic": "SELF STUDY",
+                        "subtopics": ss,
+                        "minimum_hours": None,
+                        "self_study": True
+                    })
+
+        return plan_rows
