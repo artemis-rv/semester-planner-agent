9ab9d88bd7ca43ed7cafaab4c4aa328e4887ac27
artemis-rv <fjayaanm07@gmail.com>
2026-01-11 12:24:15 +0530

    added main entry point function

---
+++ main.py
@@ -0,0 +1,31 @@
+import json
+from planner.models.syllabus import Subject, Unit, Topic
+from planner.engine.planner_engine import PlannerEngine
+from planner.writers.excel_writer import ExcelWriter
+
+def load_subjects(path: str):
+    with open(path) as f:
+        raw=json.load(f)
+
+    subjects=[]
+
+    for s in raw["subjects"]:
+        units=[]
+        for u in s["units"]:
+            topics=[Topic(**t) for t in u["topics"]]
+            units.append(Unit(
+                unit_no=u["unit_no"],
+                title=u["title"],
+                importance=u["importance"],
+                minimum_hours=u.get("minimum_hours"),
+                topics=topics,
+                self_study=u.get("self_study", [])
+            ))
+
+        subjects.append(Subject(
+            code=s["code"],
+            name=s["name"],
+            credits=s["credits"],
+            exam_weightage=s["exam_weightage"],
+            units=units
+        ))
