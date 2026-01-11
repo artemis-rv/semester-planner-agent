73b7d10bf092d764a00fb9fdbb7a21774fa1395a
artemis-rv <fjayaanm07@gmail.com>
2026-01-11 14:59:04 +0530

    fixed data presentation of subtopics

--- planner/writers/excel_writer.py
+++ planner/writers/excel_writer.py
@@ -21,29 +21,57 @@ class ExcelWriter:
         for cell in ws[1]:
             cell.font = Font(bold=True)
 
+        # for row in rows:
+        #     ws.append([
+        #         row["unit"],
+        #         row["importance"],
+        #         row["unit_title"],
+        #         row["topic"],
+        #         row["subtopics"] if row["topic"]=="SELF STUDY" else "",
+        #         row["minimum_hours"] or ""
+        #     ])
+
+        flag=0
         for row in rows:
-            ws.append([
-                row["unit"],
-                row["importance"],
-                row["unit_title"],
-                row["topic"],
-                row["minimum_hours"] or ""
-            ])
+            
+            if row["topic"]=="SELF STUDY" and flag==0:
+                ws.append(["","","",row["topic"],""])
+                flag+=1
+            
+            if row["topic"]=="SELF STUDY":
+                ws.append([
+                    row["unit"],
+                    row["importance"],
+                    row["unit_title"],
+                    # row["topic"],
+                    row["subtopics"] if flag==1 else row["topic"],
+                    row["minimum_hours"] or ""
+                ])
+            if row["topic"]!="SELF STUDY":
+                ws.append([
+                    row["unit"],
+                    row["importance"],
+                    row["unit_title"],
+                    row["topic"],
+                    # row["subtopics"] if row["topic"]=="SELF STUDY" else "",
+                    row["minimum_hours"] or ""
+                ])
 
         self._apply_unit_separators(ws)
 
     def _apply_unit_separators(self, ws):
         thin = Side(style="thin")
-        border = Border(bottom=thin)
+        border = Border(top=thin, bottom=thin)
 
 # ws is a worksheet
 # iter_rows() returns rows as tuples of cells
 # min_row=2 means: Skip row 1 (the header)
 
         for row in ws.iter_rows(min_row=2):
-            if row[2].value == "SELF STUDY":
-                for cell in row:
-                    cell.border = border
+            if row[3].value == "SELF STUDY":
+                row[3].font=Font(bold=True)
+                row[3].border = border
+                # for cell in row:
 
     def save(self, path: str):
         if "Sheet" in self.wb.sheetnames:
